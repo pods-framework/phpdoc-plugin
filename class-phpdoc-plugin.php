14e8af2d261628ad51f0fe30c3a1b8704b712cc7
Ronald Huereca <ronalfy@gmail.com>
2021-09-28 14:42:22 -0500

    Add main plugin file.

---
+++ class-phpdoc-plugin.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: phpDoc Plugin
+ * Plugin URI: https://github.com/pods-framework/phpdoc-plugin
+ * Description: A companion plugin for docs parsing with the PHPDoc Parser.
+ * Author: Pods Framework Team
+ * Author URI: https://pods.io/about/
+ * Version: 0.0.1
+ * Requires at least: 5.8
+ * Text Domain: phpdoc-plugin
+ *
+ * @package phpdocplugin
+ */
+
+namespace Pods\phpdocplugin;
+
+define( 'PHPDOCPLUGIN_FILE', __FILE__ );
+define( 'PHPDOCPLUGIN_VERSION', '0.0.1' );
+
+// Support for site-level autoloading.
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+/**
+ * Main plugin class.
+ */
+class PHPDoc_Plugin {
+	/**
+	 * Run when plugins have been loaded.
+	 */
+	public function plugins_loaded() {
+		$plugin = new Plugin();
+
+		// Init the plugin.
+		add_action( 'init', array( $plugin, 'init' ) );
+	}
+}
+
+$phpdoc_plugin = new PHPDoc_Plugin();
+
+add_action( 'plugins_loaded', array( $phpdoc_plugin, 'plugins_loaded' ) );
